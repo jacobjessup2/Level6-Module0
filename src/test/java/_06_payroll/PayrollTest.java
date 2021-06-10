9dc7120fc538f35e270ab9b2c12def130fab0c9d
monij <monij@192.168.1.8>
2021-06-09 17:27:59 -0700

    Payroll (finished)

--- src/test/java/_06_payroll/PayrollTest.java
+++ src/test/java/_06_payroll/PayrollTest.java
@@ -11,28 +11,36 @@ class PayrollTest {
     @Test
     void itShouldCalculatePaycheck() {
         //given
-
+    	double wage = 7.25;
+    	int hours = 5;
+    	double expected = 36.25;
         //when
-
+    	double actual = (double) payroll.calculatePaycheck(wage, hours);
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldCalculateMileageReimbursement() {
         //given
-
+    	int miles = 30;
+    	double expected = 17.25;
         //when
-
+    	double actual = payroll.calculateMileageReimbursement(miles);
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldCreateOfferLetter() {
         //given
-
+    	String name = "Jeff";
+    	double wage = 7.25;
+    	String expected = "Hello Jeff, We are pleased to offer you an hourly wage of 7.25";
         //when
-
+    	String actual = payroll.createOfferLetter(name, wage);
         //then
+    	assertEquals(expected, actual);
     }
 
 }
