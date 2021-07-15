4dc23726fbc5cb5c070e8aa8a58f9bda9ccddee3
monij <monij@192.168.1.8>
2021-07-14 17:25:46 -0700

    Mocking (finished)

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -12,37 +12,62 @@ import static org.mockito.Mockito.when;
 class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
-
+    
+    @Mock
+    Car car;
+    
+//    @Mock
+//    String name;
+    
+    @Mock
+    CellPhone cellPhone;
+    
+//    @Mock
+//    int octane;
+//    
+//    @Mock String phoneNumber;
+    
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	String name = "Jeff";
+    	deliveryDriver = new DeliveryDriver(name,car,cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
-
+    	boolean expectedWaste = true;
         //when
-
+    	when(cellPhone.browseCatMemes()).thenReturn(true);
+    	boolean actualWaste = cellPhone.browseCatMemes();
         //then
+    	assertEquals(expectedWaste,actualWaste);
     }
 
     @Test
     void itShouldRefuel() {
         //given
-
+    	boolean expectedRefuel = true;
+    	int octane = 85;
         //when
-
+    	when(car.fillTank(octane)).thenReturn(true);
+    	boolean actualRefuel = car.fillTank(octane);
         //then
+    	assertEquals(expectedRefuel, actualRefuel);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
-
+    	boolean expectedCall = true;
+    	String phoneNumber = "1111111111";
         //when
-
+    	when(cellPhone.call(phoneNumber)).thenReturn(true);
+    	boolean actualCall = cellPhone.call(phoneNumber);
         //then
+    	assertEquals(expectedCall, actualCall);
     }
 
 }
