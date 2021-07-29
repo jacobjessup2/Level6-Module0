60705a5202f225487438e1181334bb7acd3918b3
monij <monij@DESKTOP-4MC4PHB>
2021-07-28 17:27:42 -0700

    White box testing(finished)

--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -11,14 +11,21 @@ import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class MyDonutShopTest {
-	@Mock
+	
     MyDonutShop myDonutShop;
     
     @Mock
     BakeryService bakeryService;
+    
+    @Mock
+    DeliveryService deliveryService;
+    
+    @Mock
+    PaymentService paymentService;
     
 //    @Mock
 //    Order order;
@@ -26,7 +33,9 @@ class MyDonutShopTest {
     @BeforeEach
     void setUp() {
     	MockitoAnnotations.openMocks(this);
-    	
+    	myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
+    	myDonutShop.setPaymentService(paymentService);
+    	myDonutShop.setDeliveryService(deliveryService);
     }
 
     @Test
@@ -38,29 +47,51 @@ class MyDonutShopTest {
                 5.00,
                 "CREDIT_CARD_NUMBER",
                 true);
+    	when(paymentService.charge(order)).thenReturn(true);
+    	when(bakeryService.getDonutsRemaining()).thenReturn(1);
         //when
+    	myDonutShop.openForTheDay();
     	myDonutShop.takeOrder(order);
         //then
-    	verify(myDonutShop, times(1)).takeOrder(order);
+    	verify(deliveryService, times(1)).scheduleDelivery(order);
     		
     }
 
     @Test
-    void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
+    void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() throws Exception {
         //given
-    	
+    	Order order = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
+    	when(paymentService.charge(order)).thenReturn(true);
+    	when(bakeryService.getDonutsRemaining()).thenReturn(0);
         //when
-
+    	myDonutShop.openForTheDay();
+    	
         //then
+    	Throwable exceptionThrown = assertThrows(IllegalArgumentException.class, () -> myDonutShop.takeOrder(order));
+    	assertEquals(exceptionThrown.getMessage(), "Insufficient donuts remaining");
+        
     }
 
     @Test
     void givenNotOpenForBusiness_whenTakeOrder_thenThrowIllegalStateException(){
         //given
-
+    	Order order = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
+    	myDonutShop.closeForTheDay();
         //when
-
+    	
         //then
+    	Throwable exceptionThrown = assertThrows(IllegalStateException.class, () -> myDonutShop.takeOrder(order));
+    	assertEquals(exceptionThrown.getMessage(), "Sorry we're currently closed");
     }
 
 }
